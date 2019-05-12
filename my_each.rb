593351c1c06f8f1865843d911535fc1fe1b71077
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 06:35:26 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(array) #this should build out the yielf in this method
   # code here
+  i = 0
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+    array
 end
