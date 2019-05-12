15bd641f58ec5f0b32ca833b13ebe448ee368d40
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 06:34:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(array) #this should build out the yielf in this method
   # code here
   i = 0
     while i < array.lenth
-      yield array[i]
+      yield(array[i])
       i += 1
     end
 end
